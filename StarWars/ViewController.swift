b384dad8abdc88cf39f262b13055410a155e3f5a
Nikolay Trofimov <nikktro@gmail.com>
2023-06-07 18:18:32 +0300

    Initial Commit

---
+++ StarWars/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarWars
+//
+//  Created by Nikolay Trofimov on 07.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
